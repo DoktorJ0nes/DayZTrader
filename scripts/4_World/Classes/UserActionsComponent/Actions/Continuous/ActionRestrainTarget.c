79c3c4cff3be94d2278c76ab6b8a7411688c5d66
Jonas Böhm <shaizeone@web.de>
2019-05-27 23:44:32 +0200

    Fixed restrain duping bug

--- scripts/4_World/Classes/UserActionsComponent/Actions/Continuous/ActionRestrainTarget.c
+++ scripts/4_World/Classes/UserActionsComponent/Actions/Continuous/ActionRestrainTarget.c
@@ -5,6 +5,13 @@ modded class ActionRestrainTarget
         if (player.m_Trader_IsInSafezone)
             return false;
 
+        PlayerBase targetPlayer = PlayerBase.Cast(target.GetObject());
+        if (!targetPlayer)
+			return false;
+
+        if (targetPlayer.m_Trader_IsInSafezone)
+            return false;
+
 		return super.ActionCondition(player, target, item);
 	}
 }
