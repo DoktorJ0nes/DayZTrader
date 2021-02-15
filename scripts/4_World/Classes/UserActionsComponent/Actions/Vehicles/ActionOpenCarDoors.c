4b19d679eb05550d630adbb131fc05be379eeb0e
Jonas Böhm <shaizeone@web.de>
2021-02-15 22:03:40 +0100

    Compatibility for DayZ v1.11

--- scripts/4_World/Classes/UserActionsComponent/Actions/Vehicles/ActionOpenCarDoors.c
+++ scripts/4_World/Classes/UserActionsComponent/Actions/Vehicles/ActionOpenCarDoors.c
@@ -1,4 +1,9 @@
+#ifdef DAYZ_1_10
 modded class ActionOpenCarDoors
+#endif
+#ifndef DAYZ_1_10
+modded class ActionCarDoors: ActionInteractBase
+#endif
 {
     override bool ActionCondition( PlayerBase player, ActionTarget target, ItemBase item )
 	{
@@ -14,3 +19,22 @@ modded class ActionOpenCarDoors
 		return super.ActionCondition(player, target, item);
 	}
 }
+
+#ifndef DAYZ_1_10
+modded class ActionCarDoorsOutside: ActionInteractBase
+{
+    override bool ActionCondition( PlayerBase player, ActionTarget target, ItemBase item )
+	{
+        CarScript carScript;
+        if (Class.CastTo(carScript, target.GetParent()))
+        {
+            if (carScript.m_Trader_Locked && !player.IsInVehicle())
+            {
+                return false;
+            }
+        }
+		
+		return super.ActionCondition(player, target, item);
+	}
+}
+#endif
