571f42ca3b662bc5bbcf3f4fb9738d2dc34acb91
helkhiana <helkhiana@gmail.com>
2024-06-24 13:28:16 +0100

    fuck dayz

--- scripts/4_World/InventoryTransactions.c
+++ scripts/4_World/InventoryTransactions.c
@@ -1,90 +1,11 @@
-//This was created by Helkhiana to solve the CreateInInventory issues
 //All rights reserved
-//Redistribution of this code cannot be done without explicit permission from Helkhiana
 class TM_InventoryTransactions
 {
-    static EntityAI CreateItem(string classname, notnull EntityAI target, out InventoryLocationType locationType, bool createOnGroundOnFailure = true)
-    {
-        #ifdef SERVER
-        EntityAI newItemLocal = EntityAI.Cast(GetGame().CreateObjectEx(classname,  target.GetPosition(), ECE_PLACE_ON_SURFACE|ECE_LOCAL));
-        if(!newItemLocal)
-        {
-            locationType = InventoryLocationType.UNKNOWN;
-            return null;
-        }
-        EntityAI newItem = EntityAI.Cast(TryMoveLocalItem(newItemLocal, target, locationType));        
-        if(newItem)
-        {
-            return newItem;
-        }
-
-        if(createOnGroundOnFailure)
-        {
-            locationType = InventoryLocationType.GROUND;
-            GetGame().RemoteObjectTreeCreate(newItemLocal);
-            return newItemLocal;
-        }
-
-        locationType = InventoryLocationType.UNKNOWN;
-        GetGame().ObjectDelete(newItemLocal);
-        #endif
-
-		return null;
-    }
-
-    protected static EntityAI TryMoveLocalItem(EntityAI newItemLocal, notnull EntityAI target, out InventoryLocationType locationType)
-    {
-        #ifdef SERVER        
-        InventoryLocation sourceInvLoc = new InventoryLocation();
-        newItemLocal.GetInventory().GetCurrentInventoryLocation(sourceInvLoc);
-        InventoryLocation targetInvLoc = new InventoryLocation();
-		if (target.GetInventory().FindFreeLocationFor(newItemLocal, FindInventoryLocationType.CARGO | FindInventoryLocationType.ATTACHMENT, targetInvLoc))
-		{
-            //Print(string.Format("TryMoveLocalItem: Item %1 | Target %2", Object.GetDebugName(newItemLocal), Object.GetDebugName(target)));
-            //Print(targetInvLoc.DumpToString());
-            if(GameInventory.LocationCanMoveEntity(sourceInvLoc, targetInvLoc))
-            {
-                switch (targetInvLoc.GetType())
-                {
-                    case InventoryLocationType.ATTACHMENT:
-                        if(targetInvLoc.GetParent().CanReceiveAttachment(newItemLocal, targetInvLoc.GetSlot()))
-                        {
-                            if(target.GetInventory().TakeToDst(InventoryMode.LOCAL, sourceInvLoc, targetInvLoc))
-                            {
-                                locationType = InventoryLocationType.ATTACHMENT;
-                                GetGame().RemoteObjectTreeCreate(newItemLocal);
-                                return newItemLocal;
-                            }
-                        }
-                        break;
-                    case InventoryLocationType.CARGO:
-                        if(targetInvLoc.GetParent().CanReceiveItemIntoCargo(newItemLocal))
-                        {                            
-                            if(target.GetInventory().TakeToDst(InventoryMode.LOCAL, sourceInvLoc, targetInvLoc))
-                            {
-                                GetGame().RemoteObjectTreeCreate(newItemLocal);
-                                locationType = InventoryLocationType.CARGO;
-                                return newItemLocal;
-                            }
-                        }
-                        break;
-                    case InventoryLocationType.HANDS:
-                        break;
-                    default:
-                        break;
-                }
-            }
-		}
-        locationType = InventoryLocationType.UNKNOWN;
-        #endif
-
-		return null;
-    }
-
     //1. Create item local on server
-    //2. Try to move item in inventory of player directly
-    //3. Try to move in attachments if #2 failed
-    //4. Create on ground if allowed to and #3 failed
+    //2. Check it can be created. No point continuing through loop if not
+    //3. CreateInInventory of player
+    //4. Try CreateInInventory in attachments of player if #3 fails
+    //5. Create on ground if allowed to and #4 failed
     static EntityAI CreateItemInPlayerInventory(string classname, notnull PlayerBase target, out InventoryLocationType locationType, bool createOnGroundOnFailure = true)
     {        
         #ifdef SERVER
@@ -96,10 +17,17 @@ class TM_InventoryTransactions
             locationType = InventoryLocationType.UNKNOWN;
             return null;
         }
+        else
+        {
+            GetGame().ObjectDelete(newItemLocal);
+        }
 
-        EntityAI newItem = EntityAI.Cast(TM_InventoryTransactions.TryMoveLocalItem(newItemLocal, target, locationType));
+        InventoryLocation newItemInvLoc = new InventoryLocation;
+        EntityAI newItem = EntityAI.Cast(target.GetInventory().CreateInInventory(classname));
         if (newItem)
         {
+            newItem.GetInventory().GetCurrentInventoryLocation(newItemInvLoc);
+            locationType = newItemInvLoc.GetType();
             return newItem;
         }
         else
@@ -114,9 +42,11 @@ class TM_InventoryTransactions
                     {
                         continue;
                     }
-                    newItem = EntityAI.Cast(TM_InventoryTransactions.TryMoveLocalItem(newItemLocal, attachment, locationType));
+                    newItem = EntityAI.Cast(attachment.GetInventory().CreateInInventory(classname));
                     if (newItem)
                     {
+			            newItem.GetInventory().GetCurrentInventoryLocation(newItemInvLoc);
+                        locationType = newItemInvLoc.GetType();
                         return newItem;
                     }
                 }
@@ -125,12 +55,12 @@ class TM_InventoryTransactions
 
         if(createOnGroundOnFailure && !newItem)
         {
+            newItem = EntityAI.Cast(GetGame().CreateObjectEx(classname,  target.GetPosition(), ECE_PLACE_ON_SURFACE));
             locationType = InventoryLocationType.GROUND;
-            GetGame().RemoteObjectTreeCreate(newItemLocal);
-            return newItemLocal;
+            return newItem;
         }
 
-        GetGame().ObjectDelete(newItemLocal);
+        
         locationType = InventoryLocationType.UNKNOWN;
         #endif
 
